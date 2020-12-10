7fbda2cb08476c1425c80cae284fce9ff6c1b6c8
Luks3110 <lucas_bugado@hotmail.com>
2020-12-10 14:49:33 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/lucasfernandes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/lucasfernandes/workshopmongo/repository/PostRepository.java
@@ -1,10 +1,10 @@
 package com.lucasfernandes.workshopmongo.repository;
 
-import com.lucasfernandes.workshopmongo.domain.User;
+import com.lucasfernandes.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
 
 }
